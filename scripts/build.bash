ada431306a29894045cb87dab89b596c8b812557
Konnor Rogers <konnor5456@gmail.com>
2025-09-21 14:28:50 -0400

    update to use sample.bmp

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,6 +1,15 @@
 cd "`dirname "$0"`"
 cd "../"
 
-emcmake cmake -S . -B build
-cd build
+mkdir -p build
+
+source ./vendor/emscripten/emsdk_env.sh
+
+emcmake cmake -S . -B build/html
+cd build/html
 emmake make
+
+cd ../../
+cmake -S . -B build/native
+cmake --build build/native
+
